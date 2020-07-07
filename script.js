528d9db189dc2c5fef31046737212bfce2a0ab62
Lillian Won <lwon1@uci.edu>
2020-07-06 22:25:00 -0700

    Added the hover effect for each box

--- script.js
+++ script.js
@@ -5,19 +5,30 @@ function createGrid() {
     for ( let i = 0; i < 16; i++ ) {
         let row = document.createElement('div');
         row.className = 'row'; 
-        row.id = `row${i}`;
+        row.id = `row${i}`; // might not need..
 
         for ( let j = 0; j < 16; j++) {
             let col = document.createElement('div');
-            col.className = 'col'; 
-            col.id = `col${i}`;
-            col.textContent = `col${i}`;
+            // add two classes: col, addColor
+            col.className = 'col addColor';
+            col.id = `col${i}`; // might not need
+            //col.textContent = `col${i}`;
 
             row.appendChild(col);
         }
-
         gridContainer.appendChild(row);
     }
 }
 
+function addColor() {
+    this.style.backgroundColor = "blue";
+}
+
 createGrid();
+
+// Does the "hover" effect
+let pixels = document.getElementsByClassName('col');
+
+for ( let i = 0; i < pixels.length; i++ ) {
+    pixels[i].addEventListener('mouseover', addColor);
+}
